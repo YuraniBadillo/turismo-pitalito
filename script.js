3301b187d1b9bd8bada4a0fd9c4d238a6831a75a
YuraniBadillo <152352175+YuraniBadillo@users.noreply.github.com>
2026-02-27 20:53:08 -0500

    Implement language change feature in script.js
    Add language toggle functionality for headings.

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+let language = "es";
 
+function changeLanguage() {
+    if(language === "es") {
+        document.querySelector("h1").innerText = "Tourism Routes of Pitalito";
+        language = "en";
+    } else {
+        document.querySelector("h1").innerText = "Rutas Turísticas de Pitalito";
+        language = "es";
+    }
+}
